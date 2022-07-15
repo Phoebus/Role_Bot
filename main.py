ac332da2f0b7b7a6e27a6bf09d541c7ff8a99606
Phoebus Petsimeris <79011574+Phoebus@users.noreply.github.com>
2022-07-15 16:49:51 +0300

    Role_Bot_Completion

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import discord
 import os
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+
+roles_msg_id = []
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
@@ -9,7 +14,7 @@ async def on_ready():
     for guild in client.guilds:  #cycling through all guilds
         for channel in guild.text_channels: #cycling through all channels until we find the roles channel.
             if str(channel).strip() == "roles": 
-                global roles_channel_id
+                global roles_msg_id
                 roles_channel_id = channel.id #storing the channel id
                 roles_channel = client.get_channel(roles_channel_id)
                 await roles_channel.send("""
@@ -18,4 +23,52 @@ React in this message in order to take the role you need:
 1)👍 Member
 2)👨‍💻 Programmer
 """)
+                roles_msg_id.append(channel.last_message_id)
                 break
+
+@client.event
+async def on_raw_reaction_add(payload):
+    found = False
+    for i in roles_msg_id:
+        if payload.message_id == i:
+            found = True
+            break
+
+    if not found:
+        return
+
+    guild = client.get_guild(payload.guild_id)
+
+    if guild != None:
+        if payload.emoji.name == '👍':
+            role = discord.utils.get(guild.roles, name="Member")
+            await payload.member.add_roles(role)
+        if payload.emoji.name == '👨‍💻':
+            role = discord.utils.get(guild.roles, name="Programmer")
+            await payload.member.add_roles(role)
+            
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    found = False
+    for i in roles_msg_id:
+        if payload.message_id == i:
+            found = True
+            break
+
+    if not found:
+        return
+
+    guild = client.get_guild(payload.guild_id)
+    
+    if guild != None:
+        if payload.emoji.name == '👍':
+            member = guild.get_member(payload.user_id)
+            role = discord.utils.get(guild.roles, name="Member")
+            await member.remove_roles(role)
+        if payload.emoji.name == '👨‍💻':
+            member = guild.get_member(payload.user_id)
+            role = discord.utils.get(guild.roles, name="Programmer")
+            await member.remove_roles(role)
+
+client.run('OTgxNjQ3NzM0ODk5NjMwMTQw.GiFcHX.kCVR0W4FscbT1o4AinWWXj0ccW711YiPfiXz9A')
